c97f6d2a15872508cb85ac251384d9d8abc07cb3
Delshi <redstupidyt@outlook.com>
2025-11-17 03:34:52 +0300

    Fix readme displaying

--- CacheClean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-find . -name "__pycache__" -exec rm -rf {} \;
-find . -name "*.pyc" -delete;
-find . -name "__pycache__" -exec rm -rf {} \;
-find . -name "*.pyc" -delete;
-find . -name "__pycache__" -exec rm -rf {} \;
-find . -name "*.pyc" -delete;
